4816c908fc7bf428afe2a76673d05402620e342d
Ariel Cardozo <alcdari@gmail.com>
2026-05-19 20:20:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/geo2invertsion/',
+    base: '/geo2inversion/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
